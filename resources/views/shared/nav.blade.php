f9fbe1cd3d76b13344a6be66ae9ea7537d3cb167
Kevin <arpaskevin11@gmail.com>
2025-04-25 03:06:37 +0200

    made some adjustments to the navbar

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -1,24 +1,20 @@
 <nav>
     <div class="nav-icons">
         <a href="{{ route('dashboard') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Home"><img src="{{ asset('imgs/black/home.svg') }}" alt="Home icon" class="theme-icon"></span></a>
-        <a href="{{ route('feed') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Profile"><img src="{{ asset('imgs/black/feed.svg') }}" alt="Person icon" class="theme-icon"></span></a>
-        <span class="material-symbols-outlined" data-tooltip="Dark Mode" onclick="toggleTheme()">
-            <img src="{{ asset('imgs/black/theme.svg') }}" alt="Theme Toggle" class="theme-icon">
-        </span>
         @guest
             <a href="{{ route('login') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Login"><img src="{{ asset('imgs/black/login.svg') }}" alt="Login icon" class="theme-icon"></span></a>
             <a href="{{ route('register') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Register"><img src="{{ asset('imgs/black/register.svg') }}" alt="Register icon" class="theme-icon"></span></a>
         @endguest
         @auth
-            {{-- <form action="{{ route('logout') }}" method="post">
-                @csrf
-                <button style="border: none; outline: none; background: none;" type="submit" class="link-size"><span class="material-symbols-outlined" data-tooltip="Logout"><img src="{{ asset('imgs/black/logout.svg') }}" alt="Logout icon" class="theme-icon"></span></button>
-            </form> --}}
-            <a href="{{ route('profile') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Profile"><img src="{{ asset('imgs/black/person.svg') }}" alt="Person icon" class="theme-icon"></span></a>
-            <form action="{{ route('logout') }}" method="post">
-                @csrf
-                <a class="link-size" onclick="this.closest('form').submit();" data-tooltip="Logout"><span class="material-symbols-outlined"><img src="{{ asset('imgs/black/logout.svg') }}" alt="Logout icon" class="theme-icon"></span></a>
-            </form>
+        <a href="{{ route('feed') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Profile"><img src="{{ asset('imgs/black/feed.svg') }}" alt="Person icon" class="theme-icon"></span></a>
+        <a href="{{ route('profile') }}" class="link-size"><span class="material-symbols-outlined" data-tooltip="Profile"><img src="{{ asset('imgs/black/person.svg') }}" alt="Person icon" class="theme-icon"></span></a>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <a class="link-size" onclick="if (confirm('Are you sure you want to logout?')) this.closest('form').submit();" data-tooltip="Logout"><span class="material-symbols-outlined"><img src="{{ asset('imgs/black/logout.svg') }}" alt="Logout icon" class="theme-icon"></span></a>
+        </form>
         @endauth
+        <span class="material-symbols-outlined" data-tooltip="Dark Mode" onclick="toggleTheme()">
+            <img src="{{ asset('imgs/black/theme.svg') }}" alt="Theme Toggle" class="theme-icon">
+        </span>
     </div>
 </nav>
